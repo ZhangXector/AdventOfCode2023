cb22f7e02fb410cf96a668e8d547e73eb4b36aee
Cliff Kelley <cliff@bestdealspring.com>
2023-12-04 16:14:51 -0700

    Rust, you so crazy... Why aren't you calling my function when I want?

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 pub mod puzzles;
 
+use std::fs;
 use crate::puzzles::puzzle01;
 use crate::puzzles::puzzle04;
 
@@ -7,4 +8,25 @@ fn main()
 {
     //puzzle01::main();
     puzzle04::main();
+}
+
+/// Opens a file path and reads into a Vector<String> all lines for parsing
+///
+/// # Arguments
+///
+/// * `file_path` - A string slice that holds the path of the file to open
+///
+/// # Returns
+///
+/// * Vec<string> with all lines from the file
+fn get_lines(file_path: &str) -> Vec<String>
+{
+    // Fetch lines from file as Vector of String objects
+    let lines_vector: Vec<String> = fs::read_to_string(file_path)
+        .expect("Problem opening the file: {file_path}")
+        .lines()
+        .map(String::from)
+        .collect();
+
+    return lines_vector;
 }
--- src/puzzles/puzzle04.rs
+++ src/puzzles/puzzle04.rs
@@ -1,21 +1,47 @@
-use std::fs;
+use crate::get_lines;
 
 pub fn main()
 {
     let file_path = "./04/example_input.txt";
 
-    let lines_vector: Vec<String> = fs::read_to_string(file_path)
-        .expect("Problem opening the file: {file_path}")
-        .lines()
-        .map(String::from)
-        .collect();
+    let lines_vector = get_lines(file_path);
 
+    // Iterate over lines
     for line in lines_vector
     {
+        let mut card_points = 0u32;
+
+        // Split line on : and |
         let mut line_split = line.split(&[':', '|'][..]);
-        let card = line_split.next().expect("Unable to split card numbers.").trim();
-        let winning_nums = line_split.next().expect("Unable to split winning numbers.").trim();
-        let numbers = line_split.next().expect("Unable to split numbers.").trim();
-        println!("{}\n{}\n{}", card, winning_nums, numbers);
+
+        // Get winning numbers Vector
+        let mut winning_nums: Vec<&str> = line_split
+            .next()
+            .expect("Unable to split winning numbers.")
+            .trim()
+            .split(" ")
+            .collect();
+
+        // Get card numbers Vector
+        let mut card_numbers: Vec<&str> = line_split
+            .next()
+            .expect("Unable to split numbers.")
+            .trim()
+            .split(" ")
+            .collect();
+        // Remove blank string items from splitting
+        winning_nums.retain(|&n| n != "");
+        card_numbers.retain(|&n| n != "");
+
+        println!("Winning Numbers:");
+        for win_num in winning_nums
+        {
+            println!("{}", win_num);
+        }
+        println!("Card Numbers:");
+        for card_num in card_numbers
+        {
+            println!("{}", card_num);
+        }
     }
 }
